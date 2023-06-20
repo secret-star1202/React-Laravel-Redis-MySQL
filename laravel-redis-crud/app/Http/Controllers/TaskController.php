445b597128d2f6e5f09505816ead624cebeef420
secret-star1202 <starDev1202@outlook.com>
2023-06-21 01:03:39 +0600

    task create,edit,delete part done with redis functionality (checked by postman)

--- laravel-redis-crud/app/Http/Controllers/TaskController.php
+++ laravel-redis-crud/app/Http/Controllers/TaskController.php
@@ -4,62 +4,81 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class TaskController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $tasksFromRedis = json_decode(Redis::get('tasks'));
+
+
+        if ($tasksFromRedis) {
+            foreach ($tasksFromRedis as $task) {
+                $task->source = 'Redis';
+            }
+        } else {
+            $tasksFromDatabase = Task::all();
+
+            foreach ($tasksFromDatabase as $task) {
+                $task->source = 'Database';
+            }
+
+            Redis::set('tasks', json_encode($tasksFromDatabase));
+
+            $tasksFromRedis = $tasksFromDatabase;
+        }
+
+        return response()->json($tasksFromRedis);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required',
+        ]);
+
+        $task = Task::create($request->all());
+
+        $tasks = Task::all();
+
+        Redis::del('tasks.');
+
+        return response()->json($task, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Task $task)
+    public function update(Request $request, $id)
     {
-        //
+        $task = Task::find($id);
+
+        if (!$task) {
+            return response()->json(['message' => 'Task not found'], 404);
+        }
+
+        $task->title = $request->title;
+        $task->save();
+
+        Redis::del('tasks');
+        $tasks = Task::all();
+        Redis::set('tasks', json_encode($tasks));
+
+        return response()->json($task);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Task $task)
+    public function destroy($id)
     {
-        //
-    }
+        $task = Task::find($id);
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Task $task)
-    {
-        //
-    }
+        if (!$task) {
+            return response()->json(['message' => 'Task not found'], 404);
+        }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Task $task)
-    {
-        //
+        $task->delete();
+
+        Redis::del('tasks');
+        $tasks = Task::all();
+        Redis::set('tasks', json_encode($tasks));
+
+        return response()->json(null, 204);
     }
 }
